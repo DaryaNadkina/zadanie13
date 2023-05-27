7ddce439129de4f0e0573c40597320b835377c51
Darya <dgrizz42@gmail.com>
2023-05-27 17:45:56 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace zadanie13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.buttonCreate.Location = new System.Drawing.Point(226, 143);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(422, 39);
+            this.buttonCreate.TabIndex = 1;
+            this.buttonCreate.Text = "Создать 2 упорядоченных массива";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBox10.Location = new System.Drawing.Point(151, 45);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(556, 32);
+            this.textBox10.TabIndex = 3;
+            // 
+            // textBox17
+            // 
+            this.textBox17.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBox17.Location = new System.Drawing.Point(151, 80);
+            this.textBox17.Name = "textBox17";
+            this.textBox17.ReadOnly = true;
+            this.textBox17.Size = new System.Drawing.Size(556, 32);
+            this.textBox17.TabIndex = 4;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBoxResult.Location = new System.Drawing.Point(61, 222);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.Size = new System.Drawing.Size(789, 32);
+            this.textBoxResult.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 331);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textBox17);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.buttonCreate);
+            this.Name = "Form1";
+            this.Text = "задание 13";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.TextBox textBoxResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadanie13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Create(int[] array)
+        {
+            Random r = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = r.Next(0, 31);
+            Array.Sort(array);
+        }
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            textBox10.Clear();
+            textBox17.Clear();
+            textBoxResult.Clear();
+            int[] array10 = new int[10], array17 = new int[17], arrayResult = new int[27];
+            int k = 0, i, j;
+            Create(array10);
+            Create(array17);
+            Array.Reverse(array17);
+            i = 0; j = 16;
+            arrayResult[k++] = array10[i] < array17[j] ? array10[i++] : array17[j--];
+            int x;
+            for (; i < 10 && j >= 0;)
+            {
+                x = array10[i] < array17[j] ? array10[i++] : array17[j--];
+                if (arrayResult[k - 1] != x)
+                {
+                    arrayResult[k++] = x;
+                }
+            }
+
+            if (i == 10)
+                for (; j >= 0; j--)
+                {
+                    x = array17[j];
+                    if (arrayResult[k - 1] != x)
+                        arrayResult[k++] = x;
+                }
+            else
+                for (; i < 10; i++)
+                {
+                    x = array10[i];
+                    if (arrayResult[k - 1] != x)
+                        arrayResult[k++] = x;
+                }
+            Show(textBox10, array10, 10);
+            Show(textBox17, array17, 17);
+            Show(textBoxResult, arrayResult, k);
+        }
+        private void Show(TextBox textBox,int[] array,int k)
+        {
+            for (int i = 0; i < k; i++)
+                textBox.Text += array[i].ToString() + " ";
+        }
+    }
+}
